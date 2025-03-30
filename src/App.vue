50e0d027fcfbbb3430731aef7f9c0384f280dcb8
donk666-lang <mrliu4311@gmail.com>
2025-03-30 23:09:56 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+    <dv-full-screen-container>
+      <router-view />
+    </dv-full-screen-container>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
